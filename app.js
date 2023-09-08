df4f68368e41ab961af213da4202d263da2872e3
caleb-ola <olajiire2@gmail.com>
2023-09-08 12:38:43 +0100

    fix: Implemented environment variables for urls

--- app.js
+++ app.js
@@ -7,20 +7,30 @@ app.use(express.json());
 
 const currentDate = new Date();
 
-app.get("/api/v1/details", (req, res) => {
-  const slack_name = req.query.slack_name;
-  const track = req.query.track;
-  const current_day = daysOfWeek[currentDate?.getDay()];
-  //   const utc_time = currentDate.toUTCString();
-  const utc_time = currentDate.toISOString();
+app.get("/api/v1/details", async (req, res) => {
+  try {
+    const slack_name = req.query.slack_name;
+    const track = req.query.track;
+    const current_day = daysOfWeek[currentDate?.getDay()];
+    //   const utc_time = currentDate.toUTCString();
+    const utc_time = currentDate.toISOString();
+    const github_file_url = process.env.GITHUB_FILE;
+    const github_repo_url = process.env.GITHUB_REPO;
 
-  res.status(200).json({
-    slack_name,
-    current_day,
-    utc_time,
-    track,
-    statusCode: 200,
-  });
+    res.status(200).json({
+      slack_name,
+      current_day,
+      utc_time,
+      track,
+      github_file_url,
+      github_repo_url,
+      statusCode: 200,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Something went wrong, please try again",
+    });
+  }
 });
 
 module.exports = app;
